3b2496f22a2f4b7ff90497a2925dc2e3379be834
JeffJacobson <JeffJacobson@github.fake>
2014-01-15 15:08:13 -0800

    Moved map creation into separate function.

--- Scripts/index.js
+++ Scripts/index.js
@@ -12,77 +12,83 @@ requirejs.config({
 // Check to make sure the browser supports WebWorkers. If it doesn't, inform the user that they need to upgrade.
 require(["leaflet"], function (L) {
 	"use strict";
-	var map, osmLayer, mapQuestOsmLayer, mapQuestOALayer, openCycleMapLayer, ocmTransportLayer, ocmLandscapeLayer,
-		ocmOutdoorsLayer, osmAttrib, mqAttrib, ocmAttrib, layerList;
 
-	// Define attribution strings that are common to multiple basemap layers.
-	osmAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
-	mqAttrib = osmAttrib + '<p>Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></p>';
-	ocmAttrib = osmAttrib + '<p>Tiles Courtesy of <a href="http://www.thunderforest.com/" target="_blank">Thunderforest</a></p>';
+	var map;
 
-	// Create the basemap layers.
-	osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: osmAttrib,
-		maxZoom: 18
-	});
+	function createMap() {
 
-	mapQuestOsmLayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
-		subdomains: ["otile1", "otile2", "otile3", "otile4"],
-		attribution: mqAttrib,
-		maxZoom: 18
-	});
+		var osmLayer, mapQuestOsmLayer, mapQuestOALayer, openCycleMapLayer, ocmTransportLayer, ocmLandscapeLayer,
+			ocmOutdoorsLayer, osmAttrib, mqAttrib, ocmAttrib, layerList;
 
-	mapQuestOALayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
-		subdomains: ["otile1", "otile2", "otile3", "otile4"],
-		attribution: mqAttrib,
-		maxZoom: 18
-	});
+		// Define attribution strings that are common to multiple basemap layers.
+		osmAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
+		mqAttrib = osmAttrib + '<p>Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></p>';
+		ocmAttrib = osmAttrib + '<p>Tiles Courtesy of <a href="http://www.thunderforest.com/" target="_blank">Thunderforest</a></p>';
 
-	mapQuestOALayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
-		subdomains: ["otile1", "otile2", "otile3", "otile4"],
-		attribution: mqAttrib,
-		maxZoom: 18
-	});
+		// Create the basemap layers.
+		osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: osmAttrib,
+			maxZoom: 18
+		});
 
-	openCycleMapLayer = L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
-		attribution: ocmAttrib,
-		maxZoom: 18
-	});
+		mapQuestOsmLayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+			subdomains: ["otile1", "otile2", "otile3", "otile4"],
+			attribution: mqAttrib,
+			maxZoom: 18
+		});
 
-	ocmTransportLayer = L.tileLayer('http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png', {
-		attribution: ocmAttrib,
-		maxZoom: 18
-	});
+		mapQuestOALayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
+			subdomains: ["otile1", "otile2", "otile3", "otile4"],
+			attribution: mqAttrib,
+			maxZoom: 18
+		});
 
-	ocmLandscapeLayer = L.tileLayer('http://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png', {
-		attribution: ocmAttrib,
-		maxZoom: 18
-	});
+		mapQuestOALayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg', {
+			subdomains: ["otile1", "otile2", "otile3", "otile4"],
+			attribution: mqAttrib,
+			maxZoom: 18
+		});
 
-	ocmOutdoorsLayer = L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
-		attribution: ocmAttrib,
-		maxZoom: 18
-	});
+		openCycleMapLayer = L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
+			attribution: ocmAttrib,
+			maxZoom: 18
+		});
 
-	// Create the map. If geolocation is supported, it will be zoomed to the user's current location.
-	map = L.map('map', {
-		center: [47.41322033015946, -120.80566406246835],
-		zoom: 7,
-		layers: [osmLayer]
-	}).locate({ setView: true, maxZoom: 16 });
+		ocmTransportLayer = L.tileLayer('http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png', {
+			attribution: ocmAttrib,
+			maxZoom: 18
+		});
 
-	// Create the layer list control and add it to the map.
-	layerList = L.control.layers({
-		OpenStreetMap: osmLayer,
-		"MapQuest OSM": mapQuestOsmLayer,
-		"MapQest Open Aerial": mapQuestOALayer,
-		"OpenCycleMap": openCycleMapLayer,
-		"OpenCycleMap Transport": ocmTransportLayer,
-		"OpenCycleMap Landscape": ocmLandscapeLayer,
-		"OpenCycleMap Outdoors": ocmOutdoorsLayer
-	}).addTo(map);
+		ocmLandscapeLayer = L.tileLayer('http://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png', {
+			attribution: ocmAttrib,
+			maxZoom: 18
+		});
 
+		ocmOutdoorsLayer = L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
+			attribution: ocmAttrib,
+			maxZoom: 18
+		});
 
+		// Create the map. If geolocation is supported, it will be zoomed to the user's current location.
+		map = L.map('map', {
+			center: [47.41322033015946, -120.80566406246835],
+			zoom: 7,
+			layers: [osmLayer]
+		}).locate({ setView: true, maxZoom: 16 });
+
+		// Create the layer list control and add it to the map.
+		layerList = L.control.layers({
+			OpenStreetMap: osmLayer,
+			"MapQuest OSM": mapQuestOsmLayer,
+			"MapQest Open Aerial": mapQuestOALayer,
+			"OpenCycleMap": openCycleMapLayer,
+			"OpenCycleMap Transport": ocmTransportLayer,
+			"OpenCycleMap Landscape": ocmLandscapeLayer,
+			"OpenCycleMap Outdoors": ocmOutdoorsLayer
+		}).addTo(map);
+
+		return map;
+	}
 
 	/**
 	 * @typedef {Object} Event
@@ -130,6 +136,8 @@ require(["leaflet"], function (L) {
 		req.open("get", createReverseGeocodeRequestUrl(e.latlng));
 		req.send();
 	}
+
+	map = createMap();
 
 	// Setup map on-click event.
 	map.on('click', handleMapClick);
